4c154ef88cdd3c5042e54e475422024ed011a957
Oscar García <102832100+OscarTec98@users.noreply.github.com>
2023-05-25 20:16:47 -0700

    Update Ejercicio6.py

--- Code/Ejercicio6.py
+++ Code/Ejercicio6.py
@@ -1,3 +1,5 @@
+#García Vázquez Oscar Daniel - 18212189
+
 #Programa que calcula el valor de Z dada la funcion -> Z = (a+b)^3 - (x+y)^2 (a-c)^4 + 1/2(c+x)
 #Donde a = 4,  b = 6, x = 8, y = 2 , c = 5
 
